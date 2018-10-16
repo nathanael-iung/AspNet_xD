447376c48a0afe06cd2ba9f0fe6e7346bf3a20bb
clovis <aspnet2017p4a@yandex.com>
2018-10-16 19:10:43 -0300

    2018-10-11

--- WebApplication4/Data/Usuario.cs
+++ WebApplication4/Data/Usuario.cs
@@ -13,5 +13,12 @@ namespace WebApplication4.Data
         public string Email { get; set; }
         public string DataNascimento { get; set; }
         public string Cpf { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Uf { get; set; }
     }
 }
