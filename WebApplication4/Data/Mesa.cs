516a8988c3bc4f6856bcba6821afd2fc0713799a
nathanael-iung <nathanaeliung@hotmail.com>
2018-11-08 18:59:05 -0200

    Teste.

--- WebApplication4/Data/Mesa.cs
+++ WebApplication4/Data/Mesa.cs
@@ -12,7 +12,7 @@ namespace WebApplication4.Data
         public ICollection<MesaUsuario> MesasUsuarios { get; set; }
     }
     public class MesaUsuario {
-        public int MesaId { get; set; }
+        public int MesaId { get; set; } 
         public Mesa Mesa { get; set; }
         public int UsuarioId { get; set; }
         public Usuario Usuario { get; set; }
